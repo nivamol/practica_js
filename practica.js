4dae83697ef265f954c36ae67d7cecd5f3c7ddaa
nivamol <nicolas05valencia@gmail.com>
2020-04-21 13:10:24 -0500

    condicionales if - else if - else

--- practica.js
+++ practica.js
@@ -1,6 +1,26 @@
-let grav_tierra = 9.8, grav_marte = 3.7, 
-    grav_jupiter = 24.8, peso = 68, peso_final;
+let grav_tierra = 9.8;
+let grav_marte = 3.7;
+let grav_jupiter = 24.8;
+let peso_final;
+let pregunta = prompt('¿Peso en marte o jupiter?')
+let usuario = prompt('¿cuál es tu peso?');
+let peso = Number(usuario);
 
-peso_final = peso * grav_marte / grav_tierra;
-peso_final = parseInt(peso_final);
-document.write(`${peso_final} MS cuadrados`);
+if(pregunta === "marte") {
+    peso_final = usuario * grav_marte / grav_tierra;
+    peso_final = parseInt(peso_final)
+    document.write(`tu peso en marte es de: ${peso_final} kilogramos`);
+}
+else if (pregunta === "jupiter") {
+    peso_final = usuario * grav_jupiter / grav_tierra;
+    peso_final = parseInt(peso_final);
+    document.write(`tu peso en jupiter es de: ${peso_final} kilogramos`)
+}
+
+else {
+    peso_final = grav_jupiter * 1000;
+    peso_final = Number(peso_final)
+    document.write(`tu peso en otro planeta es de ${peso_final} kilogramos ¡Cerdo!`);
+}
+
+
