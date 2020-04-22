8a7441a14558a6ed6cd96634d83b3ed8af637cbb
nivamol <nicolas05valencia@gmail.com>
2020-04-22 16:28:44 -0500

    dibujando 2 lineas en canvas

--- practica.js
+++ practica.js
@@ -1,4 +1,17 @@
-let ruta = window.location;
+// -------- Canva
 
-console.log(ruta);
-document.write(`estas en: ${ruta}`)
+let d = document.getElementById("dibujito");
+let lienzo = d.getContext('2d'); 
+console.log(lienzo);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+dibujarLinea("green", 100, 300, 210, 10);
+dibujarLinea("blue",10, 210, 300, 100);
