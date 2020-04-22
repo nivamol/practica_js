ac71dab53201dd668e588b96f7090ac91c9b3b6d
nivamol <nicolas05valencia@gmail.com>
2020-04-22 15:29:28 -0500

    variables, metodos, propiedades, etc

--- practica.js
+++ practica.js
@@ -1,30 +1,4 @@
-let grav_tierra = 9.8;
-let grav_marte = 3.7;
-let grav_jupiter = 24.8;
-let peso_final;
-let pregunta = prompt('¿Peso en marte o jupiter?')
-let usuario = prompt('¿cuál es tu peso?');
-let peso = Number(usuario);
+let ruta = window.location;
 
-if(pregunta === "marte") {
-    peso_final = parseInt(usuario * grav_marte / grav_tierra);
-}
-else if (pregunta === "jupiter") {
-    peso_final = parseInt(usuario * grav_jupiter / grav_tierra);
-}
-
-else {
-    pregunta = "krypton";
-    peso_final = 1000000;
-}
-
-document.write(`tu peso en ${pregunta} es de ${peso_final} kilogramos`)
-
-/* else {
-    peso_final = grav_jupiter * 1000;
-    peso_final = Number(peso_final)
-    document.write(`tu peso en otro planeta es de ${peso_final} kilogramos ¡Cerdo!`);
-}
-
-*/
-
+console.log(ruta);
+document.write(`estas en: ${ruta}`)
