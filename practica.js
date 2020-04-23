95f7509cf86cdfe15494a3f43eff1f33b47602da
nivamol <nicolas05valencia@gmail.com>
2020-04-22 20:59:29 -0500

    ciclos while

--- practica.js
+++ practica.js
@@ -2,7 +2,22 @@
 
 let d = document.getElementById("dibujito");
 let lienzo = d.getContext('2d'); 
-console.log(lienzo);
+let lineas = 30;
+let contador = 0;
+let yi;
+let xf;
+let colorcito = "#000";
+
+
+while(contador < lineas) {
+    yi = 10 * contador;
+    xf = 10 * (contador + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    contador++;
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 299);
+dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
     lienzo.beginPath();
@@ -12,6 +27,3 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
     lienzo.stroke();
     lienzo.closePath();
 }
-
-dibujarLinea("green", 100, 300, 210, 10);
-dibujarLinea("blue",10, 210, 300, 100);
