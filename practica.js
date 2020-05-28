16f4cd01f61ee964f4ea192a963f22b85643bff7
nivamol <nicolas05valencia@gmail.com>
2020-05-28 16:41:08 -0500

    repaso

--- practica.js
+++ practica.js
@@ -294,47 +294,56 @@ console.log(`al final del año ${nicolas.nombre} pesó ${nicolas.peso.toFixed(1)
 
 */
 
-let nicolas = {
-    nombre: "nicolas",
-    apellido: "valencia",
-    edad: 23,
-    peso: 75
-} 
+// let nicolas = {
+//     nombre: "nicolas",
+//     apellido: "valencia",
+//     edad: 23,
+//     peso: 75
+// } 
 
 
-let valeria = {
-    nombre: "valeria",
-    apellido: "valencia",
-    edad: 24,
-    peso: 65
-} 
+// let valeria = {
+//     nombre: "valeria",
+//     apellido: "valencia",
+//     edad: 24,
+//     peso: 65
+// } 
 
 
-const variacionPeso = 0.3;
+// const variacionPeso = 0.3;
 
-const AUMENTAR_PESO = persona => persona.peso += variacionPeso;
+// const AUMENTAR_PESO = persona => persona.peso += variacionPeso;
 
-const ADELGAZAR = persona => persona.peso -= variacionPeso;
+// const ADELGAZAR = persona => persona.peso -= variacionPeso;
 
-const COME_MUCHO = () => Math.random() < 0.3;
+// const COME_MUCHO = () => Math.random() < 0.3;
 
-const REALIZA_DEPORTE = () => Math.random() < 0.4;
+// const REALIZA_DEPORTE = () => Math.random() < 0.4;
 
 
 
-const DIAS_DEL_AÑO = 365;
+// const DIAS_DEL_AÑO = 365;
 
-const META = nicolas.peso - 3; 
+// const META = nicolas.peso - 3; 
 
-let dias = 0; 
+// let dias = 0; 
 
-while (nicolas.peso > META) {
-    if (COME_MUCHO) {
-        AUMENTAR_PESO(nicolas);
-    } if (REALIZA_DEPORTE){
-        ADELGAZAR(nicolas);
-    }
-    dias += 1;
+// while (nicolas.peso > META) {
+//     if (COME_MUCHO) {
+//         AUMENTAR_PESO(nicolas);
+//     } if (REALIZA_DEPORTE){
+//         ADELGAZAR(nicolas);
+//     }
+//     dias += 1;
+// }
+
+// console.log(`pasaron ${dias} dias para que ${nicolas.nombre} adelgazara 3KG`);
+
+
+let edad = Number(prompt("¿Cuál es su edad?"))
+
+if (edad >= 18) {
+    document.write('Usted es mayor de edad, puede votar');
+} else {
+    document.write('Anda por el tetero mocoso');
 }
-
-console.log(`pasaron ${dias} dias para que ${nicolas.nombre} adelgazara 3KG`);
